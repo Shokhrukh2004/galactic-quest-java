120af1159a3dd586968c2b8ffae7a74ee46bbf48
Shokhrukh Ernazarov <shokhrukhernazarov@mail.ru>
2025-03-22 15:56:23 +0100

    added initial abstract class GameObject

--- src/main/java/org/github/galacticquest/models/GameObject.java
+++ src/main/java/org/github/galacticquest/models/GameObject.java
@@ -1,4 +1,26 @@
 package org.github.galacticquest.models;
 
-public class GameObject {
+public abstract class GameObject {
+
+    private int positionX;
+    private int positionY;
+
+    abstract void update();
+    abstract void draw();
+
+    public int getPositionX(){
+        return this.positionX;
+    }
+
+    public void setPositionX(int positionX){
+        this.positionX = positionX;
+    }
+
+    public int getPositionY(){
+        return this.positionY;
+    }
+
+    public void setPositionY(int positionY){
+        this.positionY = positionY;
+    }
 }
